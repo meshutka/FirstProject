b7b7c2ecbacf598f11e184a3663103a0dac2fa1b
Student <Student@EMIS.local>
2018-02-22 16:04:30 +0700

    fht

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+          
             int a = 25;
+            int b = 2 * a;
             Console.WriteLine("a");
         }
     }
